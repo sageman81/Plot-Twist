18ee6ffa5ee92a6a8d7a62b36d9f8eac58887acf
Sageman81 <sagemichael1@gmail.com>
2024-06-10 12:25:45 -0400

    Configure database for Heroku Postgress

--- plot_twist/settings.py
+++ plot_twist/settings.py
@@ -19,7 +19,7 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
